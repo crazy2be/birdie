f34a189f0ab0cfe353b28e8f31c89a4cffbd2fbe
crazy2be <crazy1be@gmail.com>
2012-01-19 17:45:33 -0500

    Trying to get the Write side of integer encoding working.. Might just switch to using a single bit from every byte.

--- types.go
+++ types.go
@@ -2,6 +2,7 @@ package birdie
 
 import (
 	"io"
+	"fmt"
 )
 
 func readByte(rd io.Reader) (byte, error) {
@@ -38,6 +39,24 @@ func readUint32(rd io.Reader) (uint32, error) {
 	return uint32(s1) << 16 | uint32(s2), nil
 }
 
+// next7BitByte reads a single byte from rd, shifts *num up by 7, and stores the low 7 bytes of the newly read byte in the now-freed low 7 bits of *num. Returns if the high bit was set (and we should continue reading), and the error, if any.
+func next7BitByte(rd io.Reader, num *uint64) (bool, error) {
+	b, err := readByte(rd)
+	if err != nil {
+		return false, err
+	}
+	
+	digits := uint64(b & 0x7F)
+	*num = *num << 7;
+	*num |= digits;
+	
+	if (b & 0x80 == 0x80) {
+		return true, nil
+	}
+	
+	return false, nil
+}
+
 // ReadInt reads a variable-length integer from rd and returns it and an error, if any. Encodes integers bytewise in groups sized by powers of two, with the first bit in each group signifiying if there are additional byte groups to be read.
 // I.e. in the first byte of a group, 0b1xxxxxxx signifies that the next byte should be read, while 0b0xxxxxxx signifies that this is the end of the integer.
 // An example sequence of bytes might be:
@@ -46,28 +65,16 @@ func readUint32(rd io.Reader) (uint32, error) {
 func ReadInt(rd io.Reader) (uint64, error) {
 	var num uint64
 	
-	b1, err := readByte(rd)
-	if (err != nil) {
-		return 0, err
+	// First two chunks are bytes
+	for i := 0; i < 2; i++ {
+		highBit, err := next7BitByte(rd, &num)
+		if err != nil {
+			return 0, err
+		}
+		if !highBit {
+			return num, nil
+		}
 	}
-	
-	num = uint64(b1 & 0x7F)
-	if (b1 & 0x80 != 0x80) {
-		return num, nil
-	}
-	
-	
-	b2, err := readByte(rd)
-	if err != nil {
-		return num, err
-	}
-	
-	num = num << 7
-	num |= uint64(b2 & 0x7F)
-	if (b2 & 0x80) != 0x80 {
-		return num, nil
-	}
-	
 	
 	s1, err := readUint16(rd)
 	if err != nil {
@@ -92,4 +99,87 @@ func ReadInt(rd io.Reader) (uint64, error) {
 		panic("Integers longer than 8 bytes (uint64) not implemented in ReadInt()!")
 	}
 	return num, nil
+}
+
+// Returns the number of bytes required to write the given integer to a network stream using WriteInt().
+func bytesNeeded(num uint64) int {
+	if (num < 0x7F) {
+		return 1
+	} else if (num < 0x7F7F) {
+		return 2
+	} else if (num < 0x7F7F7FFF) {
+		return 4
+	} else if (num < 0x7F7F7FFF7FFFFFFF) {
+		return 8
+	}
+	return -1
+}
+
+func writeByte(wr io.Writer, b byte) error {
+	n, err := wr.Write([]byte{b})
+	if n != 1 {
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func hex(num uint64) uint64 {
+	fmt.Printf("%d or %x\n", num, num)
+	return num
+}
+
+func writeIntBytewise(wr io.Writer, num uint64) error {
+	writeByte(wr, byte(hex((num & (0xFF << 24)) >> 24)))
+	writeByte(wr, byte(hex((num & (0xFF << 16)) >> 16)))
+	writeByte(wr, byte(hex((num & (0xFF << 8)) >> 8)))
+	writeByte(wr, byte(hex((num & (0xFF)))))
+	return nil
+}
+
+func WriteInt(wr io.Writer, num uint64) error {
+	fmt.Printf("%d or %x\n", num, num)
+	if (num & 0xFF << 8 * 7 == num) {
+		println("Only high byte is used!")
+	}
+	println("Requires", bytesNeeded(num), "bytes to encode.")
+	
+	numbytes := bytesNeeded(num)
+	if (numbytes == 1) {
+		err := writeByte(wr, byte(num))
+		return err
+	} else if (numbytes == 2) {
+		panic("Not implemented!")
+	} else if (numbytes == 4) {
+		hex(num)
+		if (num & 0x80) == 0x80 {
+			num += (0x80 & num) << 1
+			num = num & 0xFFFFFF7F
+		}
+		hex(num)
+		if (num & (0x80 << 8)) == 0x80 << 8 {
+			num += (0x08000 & num) << 1
+			num = num & 0xFFFF7F7F
+		}
+		hex(num)
+// 		if (num & 0x80000000) == 0x80000000 {
+// 			num = num & 0xFF7F7F7F
+// 			num += 0x080000000 << 1
+// 		}
+		// TODO: Check errors
+		writeIntBytewise(wr, num)
+	}
+	if (num & 0x0000007F) == num {
+		err := writeByte(wr, byte(num & 0x0000007F))
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	if (num & 0x00003FFF) == num {
+		
+	}
+	return nil
 }
--- types_test.go
+++ types_test.go
@@ -8,7 +8,7 @@ import (
 func TestRead4BitInt(t* testing.T) {
 	buf := bytes.NewBuffer([]byte{0x92, 0xA5, 0x7F, 0xFF, 0xFF, 0xFF})
 	// (((0x92 & 0x7F) << 7 | (0xA5 & 0x7F)) << 15) | (0x7FFF & 0x7FFF)
-	expected := 76742655
+	expected := uint64(76742655)
 	
 	num, err := ReadInt(buf)
 	if err != nil {
@@ -22,7 +22,7 @@ func TestRead4BitInt(t* testing.T) {
 func TestRead8BitInt(t* testing.T) {
 	buf := bytes.NewBuffer([]byte{0x92, 0xA5, 0x8F, 0xFF, 0x7F, 0xFF, 0xFF, 0xFF})
 	// (((((0x92 & 0x7F) << 7 | (0xA5 & 0x7F)) << 15) | (0x8FFF & 0x7FFF)) << 31) | (0x7FFFFFFF & 0x7FFFFFFF)
-	expected := 164742026212933631
+	expected := uint64(164742026212933631)
 	
 	num, err := ReadInt(buf)
 	if err != nil {
@@ -30,5 +30,31 @@ func TestRead8BitInt(t* testing.T) {
 	}
 	if num != expected {
 		t.Fatalf("Got incorrect number. Expected %d, got %d.", expected, num)
+	}
+}
+
+func TestWriteIntBytewise(t *testing.T) {
+	num := uint64(1908421)
+	buf := &bytes.Buffer{}
+	
+	writeIntBytewise(buf, num)
+	b := buf.Bytes()
+	result := (((((((uint64(b[0]) << 8) | uint64(b[1])) << 8) | uint64(b[2])) << 8) | uint64(b[3])))
+	if result != num {
+		t.Fatal("Got", result, "expected", num)
+	}
+}
+
+func TestWrite4ByteInt(t* testing.T) {
+	num := uint64(76742655)
+	expected := []byte{0x92, 0xA5, 0x7F, 0xFF}
+	buf := &bytes.Buffer{}
+	
+	err := WriteInt(buf, num)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(expected, buf.Bytes()) {
+		t.Fatal("Got incorrect bytes! Expected", expected, "got", buf.Bytes())
 	}
 }
